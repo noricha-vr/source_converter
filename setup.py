2881f919bd7569473eeaa6a3fb22db4f56218128
noricha-vr <noricha.vr@gmail.com>
2022-11-14 16:04:54 +0900

    version='0.1.15',

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ packages = find_packages(exclude=['tests', 'tests.*'])
 
 setup(
     name='source_converter',
-    version='0.1.14',
+    version='0.1.15',
     license='MIT',
     author="Noricha",
     author_email="noricha.vr@gmail.com",
