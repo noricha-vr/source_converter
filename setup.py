5d9aa3151b163f3852dada1652e563adbb947c91
noricha-vr <noricha.vr@gmail.com>
2022-11-29 12:14:13 +0900

    Add: UndoSharp test.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ packages = find_packages(exclude=['tests', 'tests.*'])
 
 setup(
     name='source_converter',
-    version='0.1.15',
+    version='0.1.16',
     license='MIT',
     author="Noricha",
     author_email="noricha.vr@gmail.com",
--- tests/test_github_downloader.py
+++ tests/test_github_downloader.py
@@ -1,9 +1,15 @@
+import pytest
 from source_converter import GithubDownloader
 
 
-def test_download_github_archive_and_unzip_to_file():
-    url = "https://github.com/noricha-vr/source_converter"
-    project_name = url.split("/")[-1]
-    folder_path = GithubDownloader.download_github_archive_and_unzip_to_file(url, project_name)
-    project_path = GithubDownloader.rename_project(folder_path, project_name)
-    assert project_path.exists() is True
+class TestGithubDownloader:
+
+    @pytest.mark.parametrize(('url',), [
+        ('https://github.com/noricha-vr/source_converter',),
+        ('https://github.com/vrchat-community/UdonSharp/',),
+    ])
+    def test_download_github_archive_and_unzip_to_file(self, url):
+        project_name = url.split("/")[-1]
+        folder_path = GithubDownloader.download_github_archive_and_unzip_to_file(url, project_name)
+        project_path = GithubDownloader.rename_project(folder_path, project_name)
+        assert project_path.exists() is True
